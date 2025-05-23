b6a74af9a9c30df8a396370d0f33552b638dfd0d
Di <b.dima1916@gmail.com>
2025-05-24 00:04:18 +0300

    deleted unnecessary files

--- Backend/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Backend/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3da26c6824c6ccb9933c29c3a4e7c03ff87a8b06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cf84632622e57892bede749c7f2c895971afabe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
